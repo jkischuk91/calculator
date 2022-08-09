27214050600e3300e86b2c271a8ea3b51f4b8257
Jonathon Kischuk <107976831+jkischuk91@users.noreply.github.com>
2022-08-09 16:31:38 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def mult(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+
+# Test 1
+print(add(2,2))
+
+# Test 2
+print(mult(3*6))
+
+# Test 3
+num1 = mult(15, 4)
+num2 = divide(210, 7)
+print(subtract(num1, num2)
